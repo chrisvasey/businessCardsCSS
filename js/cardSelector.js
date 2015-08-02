902462e3acfc593b39764aca063d541a26dc6493
chrisvasey <chrismvasey@gmail.com>
2015-08-03 00:15:31 +0100

    Created stage, started business card template

---
+++ js/cardSelector.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+	$("#chrisItalik").click(function(){
+		$(".card").attr({
+			id: 'chrisItalikCard'
+		})
+	});
+	$("#petersFreelance").click(function(){
+		$(".card").attr({
+			id: 'petersFreelanceCard'
+		})
+	});
+});
+
+//http://stackoverflow.com/questions/2176986/jquery-add-id-instead-of-class
